2eb121699fc14be0e11145e82c4499af9d110151
Goya Tomohiro <wasabibeef@gmail.com>
2015-10-21 21:15:31 +0900

    initial commit

---
+++ app/src/androidTest/java/usa/ten/game/tenusa/ApplicationTest.java
@@ -0,0 +1,13 @@
+package usa.ten.game.tenusa;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
