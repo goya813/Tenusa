bcebca8523c1ff93d56abfc9b4a1a2f9d3f57a1e
Goya Tomohiro <wasabibeef@gmail.com>
2015-11-16 23:36:40 +0900

    PowerUpItemの購入情報をDBに格納するメソッドを追加した

--- app/src/main/java/usa/ten/game/tenusa/status/powerup_item/PowerUpItemManager.java
+++ app/src/main/java/usa/ten/game/tenusa/status/powerup_item/PowerUpItemManager.java
@@ -3,24 +3,34 @@ package usa.ten.game.tenusa.status.powerup_item;
 import android.content.ClipData;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import usa.ten.game.tenusa.App;
 import usa.ten.game.tenusa.R;
+import usa.ten.game.tenusa.database.SqliteDAO;
+import usa.ten.game.tenusa.util.UtilAssets;
+import usa.ten.game.tenusa.util.UtilResource;
 
 /**
  * Created by goya on 15/11/03.
  */
 public class PowerUpItemManager
 {
+    private static final String CONF_FILE = "powerup_item.txt";
+
     private static PowerUpItemManager instance = new PowerUpItemManager();
 
+    private SqliteDAO mSqliteDao;
     private List<PowerUpItem> mPowerUpItemList = new ArrayList<>();
 
     private PowerUpItemManager()
     {
+        mSqliteDao = SqliteDAO.getInstance();
         loadPowerUpItem();
     }
     public static PowerUpItemManager getInstance()
@@ -48,13 +58,63 @@ public class PowerUpItemManager
         return mPowerUpItemList;
     }
 
+    public void soldItem(int itemId)
+    {
+        for (PowerUpItem item : mPowerUpItemList){
+            if (item.getItemId() == itemId){
+                item.sold();
+            }
+        }
+    }
+
+    public void save()
+    {
+        List<PowerUpItem> insertedItems = mSqliteDao.selectPowerUpItem();
+        boolean flagInserted;
+
+        for (PowerUpItem item : mPowerUpItemList) {
+            flagInserted = false;
+            for (PowerUpItem insertedItem : insertedItems) {
+                if (insertedItem.getItemId() == item.getItemId()) {
+                    mSqliteDao.updatePowerUpItem(item);
+                    flagInserted = true;
+                    break;
+                }
+            }
+            if (flagInserted == false){
+                mSqliteDao.insertPowerUpItem(item);
+            }
+        }
+    }
+
     private void loadPowerUpItem()
     {
-        Bitmap img;
-        img = BitmapFactory.decodeResource(App.getInstance().getResources(), R.mipmap.ic_launcher);
-        addPowerUpItem(new PowerUpItem(0, 10, "hinokinobou", 3, 3, img));
-        addPowerUpItem(new PowerUpItem(1, 30, "konbou", 5, 5, img));
-        addPowerUpItem(new PowerUpItem(2, 50, "tetsunoken", 7, 7, img));
-        addPowerUpItem(new PowerUpItem(3, 70, "katana", 10, 10, img));
+        List<HashMap<String, String>> items = UtilAssets.loadAssets(CONF_FILE);
+
+        for (HashMap<String, String> item : items){
+            int id = Integer.valueOf(item.get("id"));
+            int cost = Integer.valueOf(item.get("cost"));
+            String name = item.get("name");
+            int passivePower = Integer.valueOf(item.get("passive_power"));
+            int activePower = Integer.valueOf(item.get("active_power"));
+            Bitmap img = UtilResource.loadBitmap(item.get("img_name"));
+
+            addPowerUpItem(new PowerUpItem(id, cost, name, passivePower, activePower, img));
+        }
+
+        loadItemSoldUnits();
+    }
+
+    private void loadItemSoldUnits()
+    {
+        List<PowerUpItem> soldUnitsList = mSqliteDao.selectPowerUpItem();
+
+        for (PowerUpItem soldUnits : soldUnitsList){
+            for (PowerUpItem item : mPowerUpItemList){
+                if (item.getItemId() == soldUnits.getItemId()){
+                    item.setUnitsSold(soldUnits.getUnitsSold());
+                }
+            }
+        }
     }
 }
